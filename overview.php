be9d42eede9552d306ae3f669a4ddcb1c66dbc98
nicopaepke <npaepke@gmx.net>
2021-12-02 16:01:59 +0100

    tiles overview

--- overview.php
+++ overview.php
@@ -13,7 +13,31 @@
 </head>
 <body>
 
-
-
+<div class="container-fluid">
+	<div class="row justify-content-center">
+		<div class="row-column col-md-12">	
+			<div class="page-header">
+				<h2>Applikation auswählen</h2>
+			</div>
+		</div>
+	</div>
+	<div class="row justify-content-center">
+		<div class="col-md-4 col-sm-12">
+			<a href="haushaltsbuch">
+				<div class="tile">Haushaltsbuch</div>
+			</a>
+		</div>
+		<div class="col-md-4 col-sm-12">
+			<a href="login.php">
+				<div class="tile">Login</div>
+			</a>
+		</div>
+		<div class="col-md-4 col-sm-12">
+			<a href="create_user.php">
+				<div class="tile">Registrierung</div>
+			</a>
+		</div>
+	</div>
+</div>
 </body>
 </html>
