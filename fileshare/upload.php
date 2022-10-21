236a10ecdab6cc5f88b6d0e60595bed519c021f6
nicopaepke <npaepke@gmx.net>
2022-10-21 14:45:38 +0200

    upload

--- fileshare/upload.php
+++ fileshare/upload.php
@@ -3,17 +3,16 @@ if (!$_FILES["uploadingfile"]["tmp_name"]) {
     echo "Keine Datei ausgewählt";
     exit();
 } else {
-	$folderPath = "uploads/";
-	
+	$folderPath = "/var/www/html/_files/";
 	$original_file_name = $_FILES["uploadingfile"]["name"];
 	$size_raw = $_FILES["uploadingfile"]["size"];        
-	$size_as_mb = number_format(($size_raw / 1048576), 2);        
-	if (move_uploaded_file($_FILES["uploadingfile"]["tmp_name"], "$folderPath" . $_FILES["uploadingfile"]["name"] . "")) {
-		echo "$original_file_name erfolgreich hochgeladen";
+	$size_as_mb = number_format(($size_raw / 1048576), 2); 
+	
+	if (move_uploaded_file($_FILES["uploadingfile"]["tmp_name"], "$folderPath" . $_FILES["uploadingfile"]["name"])) {
+		echo "Datei erfolgreich hochgeladen";
 	}
     else {
         echo "Datei konnte nicht kopiert werden";
-        exit;
     }
 }
 ?>
